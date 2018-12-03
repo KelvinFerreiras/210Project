42ba850b45ad63fed8ffdb97c845f0674aaea13e
KelvinFerreiras <kferrei3@u.rochester.edu>
2018-12-03 12:11:53 -0500

    Added more buttons to sidebar. Created a route for main app

--- project/src/app/routes.ts
+++ project/src/app/routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { MainComponent } from './main/main.component';
+
 
 export const appRoutes: Routes = [
     {
         path: 'signup', component: UserComponent,
         children: [{ path: '', component: SignUpComponent }]
+    },
+    {
+        path: 'main', component: MainComponent,
     },
     {
         path: '', redirectTo: '/signup', pathMatch: 'full'
