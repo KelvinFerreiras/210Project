0ef21209acf1da36e1409d989e857ba0fb22bc6f
KelvinFerreiras <kferrei3@u.rochester.edu>
2018-12-03 12:25:30 -0500

    Created routing for feed

--- project/src/app/routes.ts
+++ project/src/app/routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { SignUpComponent } from './user/sign-up/sign-up.component';
 import { MainComponent } from './main/main.component';
-
+import { FeedComponent } from './main/feed/feed.component';
 
 export const appRoutes: Routes = [
     {
@@ -11,7 +11,10 @@ export const appRoutes: Routes = [
     },
     {
         path: 'main', component: MainComponent,
+        children: [{ path: 'feed', component: FeedComponent }]
+
     },
+
     {
         path: '', redirectTo: '/signup', pathMatch: 'full'
     }
