5dcb67e9f2507a2cd864caae574ec341b21894e6
PatriceDouge <patricemorice@yahoo.com>
2018-12-10 15:01:10 -0500

    Added backend of user authentication using jwt

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const passport = require('passport');
+const _ = require('lodash');
 
 const User = mongoose.model('User');
 
@@ -18,4 +20,28 @@ module.exports.register = (req,res,next) => {
                 return next(err);
         }
     });
+}
+
+//calling passport.authenticate function to check for error, 
+module.exports.authenticate = (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        //if error from passport
+        if (err) return res.status(400).json(err);
+        //if success - registered user
+        else if (user) return res.status(200).json({ "token": user.generateJwt() });
+        //if unknown ser or wrong pass
+        else return res.status(404).json(info);
+    })(req, res);
+}
+
+//user profiles method that holds user details 
+module.exports.userProfile = (req, res, next) => {
+    User.findOne({ _id: req._id },
+        (err, user) => {
+            if(!user)
+                return res.status(404).json({ status: false, message: 'User record not found' });
+            else 
+                return res.status(200).json({ status: true, user: _.pick(user,['fullName','email']) });
+        }
+    );
 }
