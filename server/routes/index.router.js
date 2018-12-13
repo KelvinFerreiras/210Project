240822ad001fb27066731067eebef190aa763ed1
IsaacXNG <xguo24@u.rochester.edu>
2018-12-13 05:39:19 -0500

    Revert ""
    This reverts commit 3566d89d8905141fb3f8873585baa132ba6a6ff9.

--- server/routes/index.router.js
+++ server/routes/index.router.js
@@ -7,7 +7,6 @@ const Post = mongoose.model('posts');
 
 const ctrlUser = require('../controllers/user.controller');
 const ctrlPost = require('../controllers/post.controller');
-const ctrlChat = require('../controllers/chat.controller');
 
 const jwtHelper = require('../config/jwtHelper');
 
@@ -18,7 +17,6 @@ router.get('/userProfile',jwtHelper.verifyJwtToken, ctrlUser.userProfile);
 
 
 router.post('/post', ctrlPost.savePost);
-//router.post('/chat', ctrlChat.saveChat);
 
 
 router.get('/test', (req, res) => {
@@ -26,7 +24,6 @@ router.get('/test', (req, res) => {
 });
 
 router.get('/posts', ctrlPost.getPost );
-//router.get('/:room',ctrlChat.getRoom);
 
 
 router.post('/addFriend', ctrlUser.addFriend);
